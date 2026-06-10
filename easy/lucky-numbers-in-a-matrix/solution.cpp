18696cc845ff86aab7a41ba8d46205341b1b4a96
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-06-10 15:07:36 +0530

    Sync solution: Lucky Numbers in a Matrix

---
+++ easy/lucky-numbers-in-a-matrix/solution.cpp
@@ -0,0 +1,84 @@
+/*
+[Description]
+Lucky Numbers in a Matrix
+https://leetcode.com/problems/lucky-numbers-in-a-matrix/submissions/2028467108/
+
+Given an m x n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
+
+A lucky number is an element of the matrix such that it is the minimum element in its row and maximum in its column.
+
+ 
+Example 1:
+
+Input: matrix = [[3,7,8],[9,11,13],[15,16,17]]
+Output: [15]
+Explanation: 15 is the only lucky number since it is the minimum in its row and the maximum in its column.
+
+Example 2:
+
+Input: matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
+Output: [12]
+Explanation: 12 is the only lucky number since it is the minimum in its row and the maximum in its column.
+
+Example 3:
+
+Input: matrix = [[7,8],[1,2]]
+Output: [7]
+Explanation: 7 is the only lucky number since it is the minimum in its row and the maximum in its column.
+
+ 
+Constraints:
+
+  m == mat.length
+  n == mat[i].length
+  1 <= n, m <= 50
+  1 <= matrix[i][j] <= 105.
+  All elements in the matrix are distinct.
+
+[Metadata]
+- Difficulty: Easy
+- Topics: Array, Matrix
+- Slug: lucky-numbers-in-a-matrix
+*/
+
+// [Solution]
+class Solution {
+public:
+    vector<int> luckyNumbers(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<int> lucky;
+        vector<int> maxi;
+        vector<int> mini;
+        int minimum=INT_MAX;
+        int maximum=0;
+        for(int l=0;l<rows;l++)
+        {
+            for(int k=0;k<cols;k++){
+                minimum=min(minimum,matrix[l][k]);
+            }
+            mini.push_back(minimum);
+            minimum=INT_MAX;
+        }
+        for(int i=0;i<cols;i++)
+        {
+            for(int j=0;j<rows;j++)
+            {
+                maximum=max(maximum,matrix[j][i]);
+            }
+            maxi.push_back(maximum);
+            maximum=0;
+        }
+        sort(maxi.begin(), maxi.end());
+        sort(mini.begin(), mini.end());
+        set_intersection(maxi.begin(), maxi.end(),
+                          mini.begin(), mini.end(),
+                          back_inserter(lucky));
+
+
+
+        return lucky;
+        
+
+    }
+};
