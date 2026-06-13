23dc040424b17c3241dda8cf25cc5b6d3280b21b
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-06-13 18:30:26 +0530

    Sync solution: Min Stack

---
+++ medium/min-stack/solution.cpp
@@ -0,0 +1,99 @@
+/*
+[Description]
+Min Stack
+https://leetcode.com/problems/min-stack/submissions/2031788778/
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+  MinStack() initializes the stack object.
+  void push(int value) pushes the element value onto the stack.
+  void pop() removes the element on the top of the stack.
+  int top() gets the top element of the stack.
+  int getMin() retrieves the minimum element in the stack.
+
+You must implement a solution with O(1) time complexity for each function.
+
+ 
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+ 
+Constraints:
+
+  -231 <= val <= 231 - 1
+  Methods pop, top and getMin operations will always be called on non-empty stacks.
+  At most 3 * 104 calls will be made to push, pop, top, and getMin.
+
+[Metadata]
+- Difficulty: Medium
+- Topics: Stack, Design
+- Slug: min-stack
+*/
+
+// [Solution]
+class MinStack {
+    stack<long long> st;
+    long long min;
+public:
+    void push(int value) {
+        if(st.empty())
+        {
+            min=value;
+            st.push(value);
+        }
+        else{
+            if(value>min)
+                st.push(value);
+            else{
+                st.push(2LL*value-min);
+                min=value;
+            }
+        }
+    }
+    
+    void pop() {
+        long long n=st.top();
+        st.pop();
+        if(n<min){
+            min=2*min-n;
+        }
+    }
+    
+    int top() {
+        long long n=st.top();
+        if(min<n)
+            return n;
+        return min;
+    }
+    
+    int getMin() {
+        return (int)min;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(value);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
