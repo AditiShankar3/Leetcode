1ef57fcb7cbe7a71085d9bcd84316ad3f6c4d1b8
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-06-13 14:56:53 +0530

    Time: 4 ms (15.86%) | Memory: 43.9 MB (17.26%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.cpp
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-        vector<char> reverseAlphabet = {'z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a'};
-        string answer = "";
-        for(int i=0;i<words.size();i++)
+        string ans;
+        ans.reserve(words.size());
+        for(string word:words)
         {
-            int sum=0;
-            for(int j=0;j<words[i].size();j++)
-            {
-                int position1=words[i][j]-'a';
-                int weight=weights[position1];
-                sum+=weight;
+            int s=0;
+            for(char c:word){
+                s+=weights[c-'a'];
             }
-            int modulo=sum%26;
-            char mp=reverseAlphabet[modulo];
-            answer+=mp;
+            ans+='z'-s%26;
         }
-        return answer;
+        return ans;
     }
 };
