a994a827799b0f1553877a840ce9961a1d4ef019
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-06-26 20:45:53 +0530

    Sync solution: Counting Bits

--- easy/counting-bits/solution.cpp
+++ easy/counting-bits/solution.cpp
@@ -1,7 +1,7 @@
 /*
 [Description]
 Counting Bits
-https://leetcode.com/problems/counting-bits/description/
+https://leetcode.com/problems/counting-bits/submissions/2046950309/
 
 Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
@@ -47,19 +47,14 @@ Follow up:
 // [Solution]
 class Solution {
 public:
-    vector<int> toBinary(vector<int>& ans,int n)
-    {
-        if(n==0)
-        {
-            ans[0]=0;
-            return ans;
-        }
-        string bin=bitset<32>(n).to_string();
-        ans[n]=count(bin.begin(),bin.end(),'1');
-        return toBinary(ans,n-1);
-    }
     vector<int> countBits(int n) {
         vector<int> ans(n+1);
-        return toBinary(ans,n);
+        int offset=1;
+        for(int i=1;i<n+1;i++){
+            if(offset*2==i)
+                offset*=2;
+            ans[i]=ans[i-offset]+1;
+        }
+        return ans;
     }
 };
