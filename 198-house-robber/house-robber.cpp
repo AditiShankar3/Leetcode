e69dd272fe894beb3d8e7bbd70eb736d113e58ed
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-07-08 11:11:46 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (89.75%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        //basically sum of non adjacent elements 
+        // either u r picking one number and the previous's previous number or u r just picking the previous number 
+        int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=0;i<n;i++){
+            int take=nums[i];
+            if(i>1)
+                take+=prev2;
+            int not_take=0+prev;
+            int curr=max(take,not_take);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
+};
