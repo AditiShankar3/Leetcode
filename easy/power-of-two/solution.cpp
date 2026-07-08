cb8f0c089074959138326181b0ef5f5ff0dbbe5f
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-07-08 11:21:54 +0530

    Sync solution: Power of Two

---
+++ easy/power-of-two/solution.cpp
@@ -0,0 +1,56 @@
+/*
+[Description]
+Power of Two
+https://leetcode.com/problems/power-of-two/submissions/2060067507/
+
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+ 
+Example 1:
+
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+
+Example 2:
+
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+
+Example 3:
+
+Input: n = 3
+Output: false
+
+ 
+Constraints:
+
+  -231 <= n <= 231 - 1
+
+ 
+Follow up: Could you solve it without loops/recursion?
+
+[Metadata]
+- Difficulty: Easy
+- Topics: Math, Bit Manipulation, Recursion
+- Slug: power-of-two
+*/
+
+// [Solution]
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1)
+            return true;
+        if(n==0)
+            return false;
+        long long temp=abs((long long)n);
+        if(n%2==0)
+            return isPowerOfTwo(n/2);
+        else 
+            return false;
+    }
+};
