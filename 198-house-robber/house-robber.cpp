50e247b80dfda1bbf7529d6b03193e82a7a25822
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-07-10 15:42:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (71.39%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -6,7 +6,7 @@ public:
         int n=nums.size();
         int prev=nums[0];
         int prev2=0;
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             int take=nums[i];
             if(i>1)
                 take+=prev2;
