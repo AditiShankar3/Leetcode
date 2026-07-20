3a7b0bd9830ba9cd34fa06f3fda68fbafff2ab81
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-07-20 15:50:34 +0530

    Time: 4 ms (30.98%) | Memory: 18.2 MB (66.35%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -3,23 +3,14 @@ public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<int> array1d;
-        vector<vector<int>> ans;
-        for (auto &row : grid)
-        {
-            for (int x : row)
-                array1d.push_back(x);
-        }
+        vector<vector<int>> ans(m,vector<int>(n));
         k %= (m * n);
-        rotate(array1d.begin(),array1d.begin()+((m*n)-k),array1d.end());
-        int count=0;
         for(int i=0;i<m;i++){
-            vector<int> temp;
             for(int j=0;j<n;j++){
-                temp.push_back(array1d[count]);
-                count++;
+                int old_index=i*n+j;
+                int new_index=(old_index+k)%(m*n);
+                ans[new_index/n][new_index%n]=grid[i][j];
             }
-            ans.push_back(temp);
         }
         return ans;
     }
