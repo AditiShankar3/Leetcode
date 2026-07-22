d8ecbe025d8641ced9e95ff5522e34fe0e43f49f
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-07-22 22:04:36 +0530

    Sync solution: Kth Largest Element in an Array

---
+++ medium/kth-largest-element-in-an-array/solution.cpp
@@ -0,0 +1,51 @@
+/*
+[Description]
+Kth Largest Element in an Array
+https://leetcode.com/problems/kth-largest-element-in-an-array/submissions/2077387110/
+
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Can you solve it without sorting?
+
+ 
+Example 1:
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+
+ 
+Constraints:
+
+  1 <= k <= nums.length <= 105
+  -104 <= nums[i] <= 104
+
+[Metadata]
+- Difficulty: Medium
+- Topics: Array, Divide and Conquer, Sorting, Heap (Priority Queue), Quickselect
+- Slug: kth-largest-element-in-an-array
+*/
+
+// [Solution]
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        //sort(nums.begin(),nums.end(),greater<int>());
+        //return nums[k-1];
+        priority_queue<
+            int,
+            vector<int>,
+            greater<int>
+        > pq;
+        for(int n:nums){
+            pq.push(n);
+            if(pq.size()>k)
+                pq.pop();
+        }
+        return pq.top();
+
+    }
+};
