18ff9b11e9fd6bef5702f366400f9f62d2df559b
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-07-25 07:31:33 +0530

    Sync solution: Maximum Product of Two Digits

--- easy/maximum-product-of-two-digits/solution.cpp
+++ easy/maximum-product-of-two-digits/solution.cpp
@@ -1,7 +1,7 @@
 /*
 [Description]
 Maximum Product of Two Digits
-https://leetcode.com/problems/maximum-product-of-two-digits/submissions/2080154938/
+https://leetcode.com/problems/maximum-product-of-two-digits/submissions/2080156459/
 
 You are given a positive integer n.
 
@@ -61,13 +61,21 @@ Constraints:
 class Solution {
 public:
     int maxProduct(int n) {
-        vector<int> digits;
+        int max1=0;
+        int max2=0;
         while(n>0){
             int d=n%10;
-            digits.push_back(d);
+            if(d>max1)
+            {
+                max2=max1;
+                max1=d;
+            }
+            else if(d>max2){
+                max2=d;
+            }
             n=n/10;
         }
-        sort(digits.begin(),digits.end(),greater<int>());
-        return digits[0]*digits[1]; 
+        //sort(digits.begin(),digits.end(),greater<int>());
+        return max1*max2;
     }
 };
