f3f2713558fcd58b1dc468d80c624a9e3045eb45
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-08-03 13:36:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (68.89%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,61 +1,15 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int alice=0;
-        int bob=0;
-        int ta=0;
-        int tb=1;
-        while(!piles.empty())
-        {
-            int n=piles.size();
-            if(ta==0) //alice's turn
-            {
-                
-                if(n==1)
-                {
-                    alice+=piles[0];
-                    piles.pop_back(); 
-                }
-                else{
-                    int first=alice+piles[0];
-                    int last=alice+piles[n-1];
-                    if(first>last){
-                        alice=first;
-                        piles.erase(piles.begin()+0);
-                    }
-                    else{
-                        alice=last;
-                        piles.pop_back();
-                    }
-                }
-                ta=1;
-                tb=0;
-            }
-            else //bob's turn
-            {
-                if(n==1)
-                {
-                    bob+=piles[0];
-                    piles.pop_back(); 
-                }
-                else{
-                    int first=bob+piles[0];
-                    int last=bob+piles[n-1];
-                    if(first<last){
-                        bob=first;
-                        piles.erase(piles.begin()+0);
-                    }
-                    else{
-                        bob=last;
-                        piles.pop_back();
-                    }
-                }
-                ta=0;
-                tb=1;
+        int n=piles.size();
+        if(n%2==0)
+            return true;
+        vector<int> dp(piles);
+        for(int i=n-2;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                dp[j]=max(piles[i]-dp[j],piles[j]-dp[j-1]);
             }
         }
-        if(bob>alice)
-            return false;
-        return true;
+        return dp[n-1]>0;
     }
 };
