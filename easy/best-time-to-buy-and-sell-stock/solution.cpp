107acc054f8dd859bfd942f4ef2a9e3e3c9a4fb9
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-08-08 11:39:43 +0530

    Sync solution: Best Time to Buy and Sell Stock

---
+++ easy/best-time-to-buy-and-sell-stock/solution.cpp
@@ -0,0 +1,53 @@
+/*
+[Description]
+Best Time to Buy and Sell Stock
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/2098715528/
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+ 
+Constraints:
+
+  1 <= prices.length <= 105
+  0 <= prices[i] <= 104
+
+[Metadata]
+- Difficulty: Easy
+- Topics: Array, Dynamic Programming
+- Slug: best-time-to-buy-and-sell-stock
+*/
+
+// [Solution]
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi=0;
+        int buy=prices[0];
+        int prof=0;
+        for(int i=1;i<=prices.size()-1;i++){
+            if(buy>prices[i])
+                buy=prices[i];
+            int p=prices[i]-buy;
+            prof=max(prof,p);
+        }
+        return prof;
+    }
+};
