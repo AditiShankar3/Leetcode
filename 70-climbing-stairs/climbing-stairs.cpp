c4435acbc25b15ea643fc0652c59234b3797b01a
Aditi Shankar <150930722+AditiShankar3@users.noreply.github.com>
2026-08-08 22:49:55 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (59.05%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,10 +3,10 @@ public:
     int climbStairs(int n) {
         if(n<=1)
             return 1;
-        int prev2=1;
         int prev1=1;
+        int prev2=1;
         for(int i=2;i<=n;i++){
-            int curr=prev2+prev1;
+            int curr=prev1+prev2;
             prev2=prev1;
             prev1=curr;
         }
